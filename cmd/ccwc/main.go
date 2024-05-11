3eb810006e425932b516f9f934b73a4e355c8af3
Michael Whatcott <mdwhatcott@users.noreply.github.com>
2024-05-11 00:12:40 -0600

    Support for filepath.Glob

--- cmd/ccwc/main.go
+++ cmd/ccwc/main.go
@@ -14,48 +14,72 @@ import (
 var Version = "dev"
 
 func main() {
+	log.SetFlags(0)
+
 	flags := flag.NewFlagSet(fmt.Sprintf("%s @ %s", filepath.Base(os.Args[0]), Version), flag.ExitOnError)
 	_ = flags.Parse(os.Args[1:])
 
 	paths := flags.Args()
-	encoder := json.NewEncoder(os.Stdout)
 	if len(paths) == 0 {
 		counts := wc.NewCounts("stdin")
 		_, err := counts.ReadFrom(os.Stdin)
 		if err != nil {
 			log.Fatal(err)
 		}
-		err = encoder.Encode(counts)
+		err = json.NewEncoder(os.Stdout).Encode(counts)
 		if err != nil {
 			log.Fatal(err)
 		}
 		return
 	}
+	failed := 0
 	aggregate := wc.NewCounts("")
-	for _, path := range paths {
-		counts := wc.NewCounts(path)
-		file, err := os.Open(path)
-		if err != nil {
-			log.Fatal(err)
+	for _, pattern := range paths {
+		matches, _ := filepath.Glob(pattern)
+		for _, path := range matches {
+			info, _ := os.Stat(path)
+			if info != nil && info.IsDir() {
+				continue
+			}
+			counts := wc.NewCounts(path)
+			ok := countFile(path, counts)
+			if !ok {
+				failed++
+			}
+			aggregate.Include(counts)
 		}
-		_, err = counts.ReadFrom(file)
-		if err != nil {
-			log.Fatal(err)
-		}
-		err = file.Close()
-		if err != nil {
-			log.Fatal(err)
-		}
-		err = encoder.Encode(counts)
-		if err != nil {
-			log.Fatal(err)
-		}
-		aggregate.Include(counts)
 	}
 	if aggregate.Files > 1 {
-		err := encoder.Encode(aggregate)
+		err := json.NewEncoder(os.Stdout).Encode(aggregate)
 		if err != nil {
 			log.Fatal(err)
 		}
 	}
+	if failed > 0 {
+		os.Exit(1)
+	}
+}
+
+func countFile(path string, counts *wc.Counts) bool {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+	_, err = counts.ReadFrom(file)
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+	err = file.Close()
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+	err = json.NewEncoder(os.Stdout).Encode(counts)
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+	return true
 }
